35870db1b84eb916eb1d6281aea71bb95d849ca3
igromanru <igromanru@yahoo.de>
2018-07-25 23:55:37 +0200

    the script is now using windows new line character as default, but also tries to get the newline char that was used in the file.

--- offsets-updater.js
+++ offsets-updater.js
@@ -1,7 +1,7 @@
-
-const endOfLine = require('os').EOL;
 const fs = require('fs');
 const path = require('path');
+
+var endOfLine = '\r\n';//require('os').EOL;
 
 if(process.argv.length > 3) {
     console.log(`Offsets.h path: ${process.argv[2]}`);
@@ -24,7 +24,12 @@ function updateOffsets(offsetsFilePath, sdkPath) {
         return;
     }
 
-    var offsetsFileArray = fs.readFileSync(offsetsFilePath, 'utf8').split(endOfLine);
+    const offsetsFile = fs.readFileSync(offsetsFilePath, 'utf8');
+    const endlineMatch = new RegExp('^.*?(\\s{1,2})$', 'gm').exec(offsetsFile);
+    if(endlineMatch && endlineMatch.length > 1) {
+        endOfLine = endlineMatch[1];
+    }
+    const offsetsFileArray = offsetsFile.split(endOfLine);
     if(offsetsFileArray && offsetsFileArray.length > 1) {
         const offsetStartRegEx = new RegExp('\/\/ :(.*?:.*?:.*?)$', 'gm');
         const offsetRegEx = /^.*?static\s+?constexpr\s+?int.*?=\s+?0x(.*?);/gm;
